01089a9e92555670f3c30c9c1f31fc5be9d73fa5
Bartlomiej Olszowka <bartlomiej.olszowka@britenet.com.pl>
2020-04-19 13:33:54 +0200

    some adition in master

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,6 +1,7 @@
 public class TestClass {
 
     public static void main(String[] args) {
-        System.out.println("Line1");
+        System.out.println("Line1 - changed in master"); //changed in master
+        System.out.println("Line 2 - add in master"); //added in master
     }
 }
