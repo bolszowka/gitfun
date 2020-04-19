cccb3cfee24cbc8de9d9c0b4e3f1324348e61ae4
Bartlomiej Olszowka <bartlomiej.olszowka@britenet.com.pl>
2020-04-19 13:31:53 +0200

    some adition in feature

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,6 +1,7 @@
 public class TestClass {
 
     public static void main(String[] args) {
-        System.out.println("Line1");
+        System.out.println("Line1 - add some in feature"); //changed in feature
+        System.out.println("Line2 - add some in feature"); //add in feature
     }
 }
