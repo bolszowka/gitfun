b52a546d05209e4399f8fee4941ed36930319471
Bartlomiej Olszowka <bartlomiej.olszowka@britenet.com.pl>
2020-04-19 13:44:14 +0200

    second adition in master

--- src/TestClass.java
+++ src/TestClass.java
@@ -4,4 +4,8 @@ public class TestClass {
         System.out.println("Line1 - changed in master"); //changed in master
         System.out.println("Line 2 - add in master"); //added in master
     }
+
+    public static void someMethod() {
+        //do nothing
+    }
 }
